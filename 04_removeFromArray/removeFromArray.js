9f60d56d4321aa8f630a8951deae4c8c17c140e8
DaleGtech <craig_tech@outlook.com>
2024-08-22 19:23:57 +0100

    completed. very difficult. lots of comments made. must revisit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) { // the function accepts 2 types of parameters:
+    // 'array': expected to be an array:
+    // '...args': The rest parameter syntax (...args) allows the function to accept an indefinite number of arguments after the array parameter.
+    let newArray = []; // create an empty array to store elements of the original array, and those to be removed.
+    array.forEach((item) => { // loop through each element (item) in the original array
+        if (!args.includes(item)) { // This conditional checks whether the current item is NOT included in the args array.
+            newArray.push(item); // If the condition in the previous step is true, this line adds (pushes) the current item to the newArray.
+        }
+    }); // end the loop
+    return newArray; // return the new array
 };
 
 // Do not edit below this line
