017a16f11b97e5c8113b30b2c2c49e92c212b00d
DaleGtech <craig_tech@outlook.com>
2024-08-26 19:19:45 +0100

    My 2nd attempts at completing the exercises. A little easier to understand this time, apart from ex 03 and 04. Try again!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let totalSum = 0; // create a variable to hold the final sum
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) { // if num1 or num2 are negative numbers return error
+        return "ERROR";
+    } 
+    else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) { // loop through the given numbers
+            totalSum += i;// on each iteration add the number to the sum
+        }
+    } else if (num2 < num1) { // if num2 larger than num1
+        for (let i = num2; i <= num1; i++) { // loop through the given numbers
+            totalSum += i;
+        }
+    }     
+        
+    return totalSum;// return the sum after finishing the loop
 
 };
 
