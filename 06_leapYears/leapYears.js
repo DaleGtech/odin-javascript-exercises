f25a40ada5ce91f67832c3de746ce23282d17d4a
DaleGtech <craig_tech@outlook.com>
2024-08-29 16:20:43 +0100

    my version seems simpler than the given solution. however, mine does not use the && operator as suggested in README.MD

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    year = parseInt(year)  // make string an integer
+    if (year % 400 == "0") { // if divisible by 400
+        return true; 
+    } else if (year % 100 == 0) { // if divisible by 100
+        return false;
+    } else if (year % 4 == 0) {// if divisible by 4   
+        return true;
+    } else { //if not
+        return false;
+    }
 };
 
 // Do not edit below this line
