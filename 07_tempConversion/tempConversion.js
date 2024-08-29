f65bf2a641c624a452cb7e9942117be2d6be931e
DaleGtech <craig_tech@outlook.com>
2024-08-29 17:00:16 +0100

    Had some difficulty with rounding to 1 decimal place. tried '.toFixed' but returned a string. used link in README.md to use Math.round(number * 10) / 10

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) { // x °F ≘ (x − 32) * 5/9⁠ °C
+  
+  f = Math.round((f - 32) * (5/9) * 10) / 10;
+  return f;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) { // x °C ≘ (x * 9/5⁠ + 32) °F
+  c = Math.round((c * 9/5 + 32) * 10) / 10;
+  return c;
 };
 
 // Do not edit below this line
